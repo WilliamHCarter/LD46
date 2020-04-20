cec02d6fecdd700b6629b71c64da26df612590c0
michaelf24601 <michaelforte11@gmail.com>
2020-04-20 19:09:14 -0400

    Finish Line

---
+++ Assets/Scripts/finishLine.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finishLine : MonoBehaviour
+{
+    public int rotateSpeed = 4;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Time.deltaTime * rotateSpeed, 0);
+    }
+}
